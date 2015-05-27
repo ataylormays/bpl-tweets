a2da2fe734c870802abad75923d20477801eea45
unknown <ataylor@HRT00490GU1995F.kih.kmart.com>
2015-05-27 14:30:56 -0500

    removed home app; functionality now in tweets

--- epl_twitter/tweets/urls.py
+++ epl_twitter/tweets/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.home, name='home'),
-	url(r'^about$', views.about, name='about'),
+	url(r'^$', views.teams, name='teams'),
 	url(r'^(?P<club_nm>[A-Za-z_]+)/$', views.club, name='club'),
 ]
--- epl_twitter/tweets/views.py
+++ epl_twitter/tweets/views.py
@@ -61,6 +61,13 @@ def about(request):
 
 	return HttpResponse(template.render(context))
 
+def teams(request):
+	template = loader.get_template('teams.html')
+	context = RequestContext(request, {
+		'club_names': club_names,
+		})
+	return HttpResponse(template.render(context))
+
 def contact(request):
 	template = loader.get_template('contact.html')
 	context = RequestContext(request, {
