d458a7346861489f957ef3f10b00404ab410e270
Aaron TaylorMays <ataylormays@Aarons-MacBook-Pro.local>
2015-09-15 22:38:10 -0500

    Completed modularization of twitteraccess. Init commit of run functionality in test_poppop.py

---
+++ resources/constants.py
@@ -0,0 +1,38 @@
+import os
+
+# 90 min game + 15 min half time + 15 min buffer = 120 min
+TOT_MINUTES = 120
+RUN_FREQ = .5
+
+# number of cols in popularity file
+NUM_COLS = TOT_MINUTES / RUN_FREQ
+
+# refresh time for getting new keys for Twitter API call
+REFRESH_TIME = 45 * 60
+
+# Twitter API endpoint
+RESOURCE_URL = "https://api.twitter.com/1.1/search/tweets.json"
+
+# phrases to remove from query results
+BANNED_PHRASES=[
+	"RT",
+	"PRE ORDER",
+	"PRE-ORDER"
+]
+
+# directories
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, "data/")
+SECRETS_DIR = os.path.join(DATA_DIR, "secrets/")
+SINCE_DIR = os.path.join(DATA_DIR, "since_ids/")
+POPULARITY_DIR = os.path.join(DATA_DIR, "popularity/")
+
+# files
+PARAMS_FILE = os.path.join(SECRETS_DIR, "params.json")
+CONSUMERS_FILE = os.path.join(SECRETS_DIR, "consumers.con")
+TOKENS_FILE = os.path.join(SECRETS_DIR, "tokens.tok")
+SECRETS_FILE = os.path.join(SECRETS_DIR, "secrets.csv")
+CLUBS_FILE = os.path.join(DATA_DIR, "twitter_clubs.csv")
+
+
+	
---
+++ resources/twitteraccess.py
@@ -0,0 +1,269 @@
+import oauth2
+import time
+import urllib
+import urllib2
+import json
+import csv
+import pickle
+import os
+
+import constants
+
+def convert_datetime(twitter_date):
+	old_fmt = '%a %b %d %H:%M:%S +0000 %Y'
+	new_fmt = '%Y-%m-%d %H:%M:%S'
+	d = time.strptime(twitter_date, old_fmt)
+	return time.strftime(new_fmt, d)
+
+
+def get_hashtags(club_nm):
+	with open(constants.CLUBS_FILE) as clubs_file:
+		clubs = csv.reader(clubs_file, delimiter=",")
+		for row in clubs:
+			if(row[0]) == club_nm:
+				print row[1].split(", ")
+
+def extract_hashtags(twitter_object):
+	if len(twitter_object) == 0:
+		return ""
+	else:
+		h_tags = twitter_object[0]["text"]
+		for h in twitter_object[1:]:
+			h_tags += ", " + h["text"]
+		return h_tags
+
+def get_since_id(club_nm):
+	club_since_file = os.path.join(constants.SINCE_DIR, club_nm.lower().replace(" ", "_") + "_since_id.txt")
+	if os.path.exists(club_since_file):
+		with open(club_since_file, 'r+') as f:
+			return f.read()
+	else:
+		return ''
+
+def get_top_id(query):
+	data = query_twitter_api(query)
+	if data["statuses"] == []:
+		print "no data"
+	else:
+		return data["statuses"][0]["id_str"]	
+
+def update_since_id(club_nm, since_id=""):
+	club_since_file = os.path.join(constants.SINCE_DIR, club_nm.lower().replace(" ", "_") + "_since_id.txt")
+	old_id = ""
+	if since_id == "":
+		since_id = get_top_id(query_bulider(club_nm))
+	if os.path.exists(club_since_file):
+		with open(club_since_file, 'r+') as f:
+			old_id = f.read()
+			f.seek(0)
+			f.truncate()
+			f.write(since_id)
+	else:
+		with open(club_since_file, 'a+') as f:
+			f.write(since_id)
+	return old_id
+
+def qb_name(club_nm):
+	if club_nm:
+		return '"' + club_nm.replace(' ', '%20') + '"'
+	return ''
+
+def qb_hashtags(hashtags):
+	if hashtags:
+		q = ''
+		for hashtag in hashtags.split(', '):
+			q += hashtag.replace('#', '%20OR%20%23')
+		return q
+	return ''
+
+def qb_handle(club_handle):
+	if club_handle:
+		return '%20OR%20from%3A' + club_handle
+	return ''
+
+def qb_exclude(phrases):
+	phrases = ['"' + word.replace(' ', '%20') + '"' for word in phrases]
+	exclusions = ''
+	for phrase in phrases:
+		exclusions += '%20-' + phrase
+	return exclusions
+
+def query_bulider(club_nm):
+	query = ""
+	with open(constants.CLUBS_FILE) as clubs_file:
+		clubs = csv.reader(clubs_file, delimiter=",")
+		for row in clubs:
+			if(row[0]) == club_nm:
+				query += qb_name(row[0])
+				query += qb_hashtags(row[1]) 
+				query += qb_handle(row[2])
+		query += qb_exclude(constants.BANNED_PHRASES)
+		return query
+
+def build_tweet_url(username, msg_id):
+	return "https://twitter.com/" + username + "/status/" + msg_id
+
+api_params = [{}]
+consumers = []
+tokens = []
+
+def build_params():
+	with open(constants.SECRETS_FILE, 'r+') as f:
+		rows = csv.reader(f)
+		secrets = [row for row in rows]
+	
+	consumers = [oauth2.Consumer(key=secret[0], secret=secret[1]) for secret in secrets]
+	tokens = [oauth2.Token(key=secret[2], secret=secret[3]) for secret in secrets] 
+
+	api_params = [{
+		"oath_version": "1.0",
+		"oauth_nonce": oauth2.generate_nonce(),
+		"oauth_timestamp": int(time.time()),
+		"lang": "en",
+		"oauth_consumer_key": consumer.key,
+		"oauth_token": token.key
+	} for consumer, token in zip(consumers, tokens)]
+
+	# write data
+	with open(constants.PARAMS_FILE, 'w') as fp:
+		json.dump(api_params, fp)
+
+	with open(constants.CONSUMERS_FILE, 'w') as fc:
+		print consumers
+		#csv.writer(f, delimiter=",").writerows(consumers)
+		#json.dump(consumers, f)
+		pickle.dump(consumers, fc)
+
+	with open(constants.TOKENS_FILE, 'w') as ft:
+		#csv.writer(f, delimiter=",").writerows(tokens)    	
+		pickle.dump(tokens, ft)
+
+def analyze_text(text):
+	APPLICATION_ID = '' # application id
+	APPLICATION_KEY = '' # application key
+	parameters = {"text": text}
+  	url = 'https://api.aylien.com/api/v1/sentiment'
+  	headers = {
+	    "Accept":                             "application/json",
+	    "Content-type":                       "application/x-www-form-urlencoded",
+	    "X-AYLIEN-TextAPI-Application-ID":    APPLICATION_ID,
+	    "X-AYLIEN-TextAPI-Application-Key":   APPLICATION_KEY
+  	}
+  	opener = urllib2.build_opener()
+	request = urllib2.Request(url, urllib.urlencode(parameters), headers)
+	response = opener.open(request);
+	return json.loads(response.read())
+
+def query_twitter_api(query, count=1, since_id="", max_id="", result_type="", index=0):
+	# read in data
+	with open(constants.PARAMS_FILE, 'r+') as fp:
+		api_params = json.load(fp)
+
+	with open(constants.TOKENS_FILE, 'r+') as ft:
+		# rows = csv.reader(f)
+		# tokens = [row for row in rows]
+		tokens = pickle.load(ft)
+
+	with open(constants.CONSUMERS_FILE, 'r+') as fc:
+		# rows = csv.reader(f)
+		# consumers = [row for row in rows]
+		consumers = pickle.load(fc)
+
+	params = api_params[index]
+	url = constants.RESOURCE_URL
+	params["q"] = query
+	params["count"] = count # number of tweets per page
+	params["since_id"] = since_id # returns tweets more recent than
+	params["max_id"] = max_id # returns tweets older than
+	params["result_type"] = result_type # popular, recent, mixed
+	req = oauth2.Request(method="GET", url=url, parameters=params)
+	signature_method = oauth2.SignatureMethod_HMAC_SHA1()
+	req.sign_request(signature_method, consumers[index], tokens[index])
+	headers = req.to_header()
+	url = req.to_url()
+	response = urllib2.Request(url)
+	print url
+	return json.load(urllib2.urlopen(response))
+		 
+def populate_popularity(club_nm, since_id="", iteration=1):
+	print club_nm
+	#build api_params 
+	build_params()
+
+	tot_time_start = time.time()
+	if since_id == "":
+		since_id = update_since_id(club_nm)
+	i = 0
+	query = query_bulider(club_nm)
+	prev_id = get_top_id(query)
+	collected_tweets = set()
+	index = 0
+	all_data = []
+	tweet_data = []
+
+	#read and edit data
+	input_file_nm = os.path.join(constants.POPULARITY_DIR, club_nm.lower().replace(' ', '_') + "_popularity.csv")
+	try:
+		with open(input_file_nm, 'rU') as f:
+			tweet_file = csv.reader(f, delimiter=",")
+			tweet_data = [row for row in tweet_file]
+	except:
+		tweet_data = []	
+
+	# breaks when no more data
+	while(True):
+		# if program has been running for > 45 minutes, get new tokens
+		if time.time() - tot_time_start > constants.REFRESH_TIME:
+			build_params()
+
+		start = time.time()
+		data = query_twitter_api(query, count=100, max_id=prev_id, 
+			since_id=since_id, index=index, result_type="mixed")
+		
+		if data["statuses"] == []:
+			print "end of data"
+			break
+		else:
+			prev_id = int(data["statuses"][-1]["id"])-1
+			i += 1
+			# empty tweet_data variable after first run
+			if i != 1:
+				tweet_data = []
+			print str(len(data["statuses"])) + " on this page"
+			for status in data["statuses"]:
+				id_str =  status["id_str"]
+				if id_str in collected_tweets:
+					continue
+				else:
+					collected_tweets.add(id_str)
+				
+				popularity = str(status["retweet_count"] + status["favorite_count"])
+				new_tweet = True
+				for row in tweet_data:
+					if(row[0] == id_str):
+						print 'amending existing tweet'
+						row[iteration] = popularity
+						new_tweet = False
+						break
+				if new_tweet:
+					content = [id_str] + ["0"]*(iteration-1) + [str(popularity)] + ["0"]*(int(constants.NUM_COLS)-iteration)
+					tweet_data += [content]
+		
+		all_data += tweet_data		
+		end = time.time()
+		print i, end-start
+		index += 1
+		index = index % 10
+
+	#write data (overwrites file)
+	with open(input_file_nm, 'w+') as f:
+		csv.writer(f, delimiter=",").writerows(all_data)
+		
+
+if __name__ == "__main__":
+	build_params()
+	q = query_bulider("Manchester United")
+	print query_twitter_api(q)
+	#test_team = "Arsenal"
+	#populate_popularity(test_team)
+	
